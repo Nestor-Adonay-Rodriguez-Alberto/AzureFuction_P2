4ae62f9f587c0ba5b718fe63f124d0c4be8815af
Nestor Rodriguez <Nestor.Adonay.Rodriguez.Alberto@gmail.com>
2025-06-08 22:05:28 -0600

    feat: Se creo la funcionalidad para crear y a la misma vez actualizar un registro

---
+++ DataAccess/Repositories/LibroRepository.cs
@@ -0,0 +1,131 @@
+﻿
+
+using DataAccess.Persistence.Tables;
+using Domain.Entidades;
+using Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Repositories
+{
+    public class LibroRepository : ILibro
+    {
+        private readonly AppDbContext _DbContext;
+
+        public LibroRepository(AppDbContext dbContext)
+        {
+            _DbContext = dbContext;
+        }
+
+
+
+
+        // SAVE - UPDATE:
+        public async Task<Libro> SaveLibro(Libro newLibro)
+        {
+            using var transaction = await _DbContext.Database.BeginTransactionAsync();
+            try
+            {
+                LibroTable? libroTable;
+
+                if (newLibro.Id == 0)
+                {
+                    libroTable = MapToTable(newLibro);
+                    _DbContext.Libro.Add(libroTable);
+                    await _DbContext.SaveChangesAsync();
+                    await transaction.CommitAsync();
+                }
+                else
+                {
+                    libroTable = await _DbContext.Libro.FirstOrDefaultAsync(x => x.Id == newLibro.Id);
+                    if (libroTable == null)
+                    {
+                        throw new InvalidOperationException($"No se encontró el LIBRO con ID {newLibro.Id} para actualizar.");
+                    }
+
+                    _DbContext.Entry(libroTable).CurrentValues.SetValues(newLibro);
+
+                    await _DbContext.SaveChangesAsync();
+                    await transaction.CommitAsync();
+                }
+
+                LibroTable? libro = await _DbContext.Libro.FirstOrDefaultAsync(x => x.Id == libroTable.Id);
+                return MapToEntity(libro!);
+            }
+            catch (DbUpdateException ex)
+            {
+                await transaction.RollbackAsync();
+                throw new InvalidOperationException("Error al querer actualizar el Libro", ex);
+            }
+            catch (Exception ex)
+            {
+                await transaction.RollbackAsync();
+                throw new InvalidOperationException("Error inesperado al procesar la operación.", ex);
+            }
+        }
+
+
+        public Task DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<(int, List<Libro>)> GetAllLibros(string search, int page, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Libro> GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        // ENTITY TO TABLE:
+        private LibroTable MapToTable(Libro newLibro)
+        {
+            LibroTable libroTable = new()
+            {
+                Id = newLibro.Id,
+                Titulo = newLibro.Titulo,
+                AutorId = newLibro.AutorId,
+                EditorialId = newLibro.EditorialId,
+            };
+
+            return libroTable;
+        }
+
+        // TABLE TO ENTITY:
+        private Libro MapToEntity(LibroTable libroTable)
+        {
+            Libro libro = new()
+            {
+                Id = libroTable.Id,
+                Titulo = libroTable.Titulo,
+                AutorId = libroTable.AutorId,
+                EditorialId = libroTable.EditorialId
+            };
+
+            if (libroTable.Autor != null)
+            {
+                libro.Autor = new()
+                {
+                    Id = libroTable.Autor.Id,
+                    Nombre = libroTable.Autor.Nombre
+                };
+            }
+
+            if (libroTable.Editorial != null)
+            {
+                libro.Editorial = new()
+                {
+                    Id = libroTable.Editorial.Id,
+                    Nombre = libroTable.Editorial.Nombre
+                };
+            }
+
+            return libro;
+        }
+
+    }
+}
--- Domain/Entidades/Libro.cs
+++ Domain/Entidades/Libro.cs
@@ -8,5 +8,8 @@ namespace Domain.Entidades
         public string Titulo { get; set; }
         public int AutorId { get; set; }
         public int EditorialId { get; set; }
+
+        public Autor Autor { get; set; } = new Autor();
+        public Editorial Editorial { get; set; } = new Editorial();
     }
 }
